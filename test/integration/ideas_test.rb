c04e19ebd3fbeaad33d5f6c7230f2d9dcf97f9d5
Valentino Espinoza <xvalentino13@gmail.com>
2015-03-06 08:07:40 -0700

    ideas show up for user

--- test/integration/ideas_test.rb
+++ test/integration/ideas_test.rb
@@ -7,4 +7,15 @@ class IdeasTest < ActionDispatch::IntegrationTest
     user = User.new
     user.ideas.new(title: "new idea", description: "do things" )
   end
+
+  test "there is an ideas page dependent on the users ideas" do 
+    user = User.create(username: "tino", password: "poop")
+    user.ideas.create(title: "idea", description: "stuff")
+    visit login_path
+    fill_in "session[username]", with: "tino"
+    fill_in "session[password]", with: "poop"
+    click_link_or_button "Log In"
+    assert page.has_content?("tino")
+    assert page.has_content?("idea")
+  end
 end
