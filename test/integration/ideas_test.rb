1f3fa21e0bb8cd7dd77e2b890991afa895805d91
Valentino Espinoza <xvalentino13@gmail.com>
2015-03-07 12:35:01 -0700

    ideas are now clickable on user page

--- test/integration/ideas_test.rb
+++ test/integration/ideas_test.rb
@@ -8,14 +8,24 @@ class IdeasTest < ActionDispatch::IntegrationTest
     user.ideas.new(title: "new idea", description: "do things" )
   end
 
-  test "there is an ideas page dependent on the users ideas" do 
+  test "there is an ideas page dependent on the users ideas" do
+    create_user_and_an_idea_for_that_user
+    assert page.has_content?("tino")
+    assert page.has_link?("idea")
+  end
+
+  test "user can show an idea" do
+    create_user_and_an_idea_for_that_user
+    click_link_or_button "idea"
+    assert page.has_content?("stuff")
+  end
+
+  def create_user_and_an_idea_for_that_user
     user = User.create(username: "tino", password: "poop")
     user.ideas.create(title: "idea", description: "stuff")
     visit login_path
     fill_in "session[username]", with: "tino"
     fill_in "session[password]", with: "poop"
     click_link_or_button "Log In"
-    assert page.has_content?("tino")
-    assert page.has_content?("idea")
   end
 end
